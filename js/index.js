a65b092edc2ca80d01569e22832e54264e2672a9
sdz28r <siddhant2807r@gmail.com>
2021-05-22 04:30:56 +0530

    minor fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ function getVaccineCalanderByPinAndDate() {
 	const pincode = document.querySelector('#pincode').value;
 	var request = new XMLHttpRequest();
 	request.open('GET', `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, true);
-	request.setRequestHeader('accept-language', 'hi_IN');
 	request.setRequestHeader('cross-origin-resource-policy', '*');
 	request.setRequestHeader('cache-control', 'max-age=0');
 	request.setRequestHeader("Access-Control-Allow-Origin", "*");
