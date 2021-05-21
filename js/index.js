e26dd76eee2aebc4690c09071929b58991e3b135
sdz28r <siddhant2807r@gmail.com>
2021-05-22 04:15:28 +0530

    minor change

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ function getVaccineCalanderByPinAndDate() {
 	var request = new XMLHttpRequest();
 	request.open('GET', `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`, true);
 	request.setRequestHeader('accept-language', 'hi_IN');
-	request.setRequestHeader('Access-Control-Allow-Origin', '*');
 	request.setRequestHeader('cross-origin-resource-policy', '*');
-	request.setRequestHeader('sec-fetch-dest', 'empty');
-	request.setRequestHeader('sec-fetch-mode', 'cors');
-	request.setRequestHeader('sec-fetch-site', 'cross-site');
 	request.setRequestHeader('cache-control', 'max-age=0');
+	request.setRequestHeader("Access-Control-Allow-Origin", "*");
+	request.setRequestHeader("Access-Control-Allow-Credentials", "true");
+	request.setRequestHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	request.setRequestHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 	request.onload = function() {
 		const data = JSON.parse(this.response);
 		console.log(data);
